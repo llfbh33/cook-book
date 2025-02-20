8caac4b87a36a67c14d12cba06410be64f4dc0b1
llfbh33 <aubriewoodbine@yahoo.com>
2025-02-19 23:53:10 -0600

    setting up display of cookbook

--- cook-book/src/App.jsx
+++ cook-book/src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import moustache from './assets/moustache.png'
-import viteLogo from '/vite.svg'
-import { Grid2, Box, Button, Typography, Autocomplete } from '@mui/material'
+import recipes from './assets/recipes'
+import Navigation from './components/navigation'
+import { Grid2, Box, Button, Typography, Autocomplete, TextField } from '@mui/material'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedRecipe, setSelectedRecipe] = useState(null)
+
+  const handleReset = () => {
+    setSelectedRecipe(null)
+  };
 
   return (
-    <>
+    <Box sx= {{ width: '100vw', height: '100vw', justifyContent: 'center'}} >
+            <Navigation />
+      <Box container sx={{ width: '85vw' }}>
+
       {/* <div> */}
         <Grid2 container spacing={2} justifyContent="center">
           <Grid2 item>
@@ -24,11 +32,61 @@ function App() {
             />
           </Grid2>
         </Grid2>
-        <Typography variant="h2">Cook Book</Typography>
-        <Autocomplete>
-          
-        </Autocomplete>
-    </>
+        <Typography variant="h2" sx={{margin: '16px' }}>Cook Book</Typography>
+        <Grid2>
+          <Autocomplete
+              options={recipes}
+              width={'100%'}
+              getOptionLabel={(option) => option.name} // Ensure it displays the recipe name
+              onChange={(event, newValue) => setSelectedRecipe(newValue)} // Update selectedRecipe
+              renderInput={(params) => (
+                  <TextField {...params} label="Select a Recipe" variant="outlined" sx={{ backgroundColor: 'white' }} />
+              )}
+          />
+          {selectedRecipe && <Button onClick={handleReset}>Reset</Button>}
+        </Grid2>
+
+        <Grid2 container spacing={2} sx={{ marginTop: '20px', justifyContent: 'center' }}>
+          {selectedRecipe ? (
+                        <Grid2 item xs={12} sm={6} md={4} >
+                        <Box
+                          sx={{
+                            height: '150px',
+                            width: '250px',
+                            border: '1px solid #ccc',
+                            borderRadius: '8px',
+                            padding: '16px',
+                          }}
+                        >
+                          <Typography variant="h5">{selectedRecipe.name}</Typography>
+                          <Typography variant="body1">Prep Time: {selectedRecipe.prepTime}</Typography>
+                          <Typography variant="body1">Cook Time: {selectedRecipe.cookTime}</Typography>
+                          <Typography variant="body1">Servings: {selectedRecipe.servings}</Typography>
+                        </Box>
+                      </Grid2>
+          ) : recipes ? (
+            recipes.map((recipe, index) => (
+              <Grid2 item xs={12} sm={6} md={4} key={index} >
+                <Box
+                  sx={{
+                    height: '150px',
+                    width: '250px',
+                    border: '1px solid #ccc',
+                    borderRadius: '8px',
+                    padding: '16px',
+                  }}
+                >
+                  <Typography variant="h5">{recipe.name}</Typography>
+                  <Typography variant="body1">Prep Time: {recipe.prepTime}</Typography>
+                  <Typography variant="body1">Cook Time: {recipe.cookTime}</Typography>
+                  <Typography variant="body1">Servings: {recipe.servings}</Typography>
+                </Box>
+              </Grid2>
+            ))
+          ) : null}
+        </Grid2>
+        </Box>
+    </Box>
   )
 }
 
---
+++ cook-book/src/assets/recipes.js
@@ -0,0 +1,261 @@
+const baseRecipe = {
+    name: '',
+    servingSize: 0,
+    servings: 0,
+    calories: 0,
+    ingredients: [
+        {
+            name: '',
+            unit: '',
+            quantity: 0,
+        },
+    ],
+    instructions: [
+        '',
+    ],
+}
+
+const recipes = [
+    {
+        name: 'Quinoa, Spinach, Avocado Fry',
+        servingSize: 0,
+        servings: 0,
+        calories: 0,
+        ingredients: [
+            {
+                name: 'Quinoa',
+                unit: 'cup',
+                quantity: 0.5,
+                optional: false,
+            },
+            {
+                name: 'Spinach',
+                unit: 'oz',
+                quantity: 4,
+                optional: false,
+            },
+            {
+                name: 'Avocado',
+                unit: 'item',
+                quantity: 1,
+                optional: false,
+            },
+            {
+                name: 'Frozen Peas',
+                unit: 'cup',
+                quantity: 1.5,
+                optional: false,
+            },
+            {
+                name: 'Capers',
+                unit: 'Tbsp',
+                quantity: 1,
+                optional: true,
+            },
+            {
+                name: 'Shreaded Chicken',
+                unit: 'cup',
+                quantity: 0.75,
+                optional: true,
+            },
+            {
+                name: 'Teriaki Sauce',
+                unit: 'Tbsp',
+                quantity: 3,
+                optional: true,
+            },
+            {
+                name: 'Salt',
+                unit: 'tsp',
+                quantity: 0.5,
+                optional: false,
+            },
+            {
+                name: 'Pepper',
+                unit: 'tsp',
+                quantity: 1,
+                optional: false,
+            },
+            {
+                name: 'Vegetable Oil',
+                unit: 'Tbsp',
+                quantity: 2,
+                optional: false,
+            },
+            {
+                name: 'Feta Cheese',
+                unit: 'Tbsp',
+                quantity: 2,
+                optional: true,
+            },
+        ],
+        instructions: [
+            'Cook the quinoa according to the package instructions.',
+            'Defrost peas by running under warm water.',
+            'Combine quinoa and peas, set aside.',
+            'In a large skillet, heat oil over medium heat.',
+        ],
+    },
+    {
+        name: 'Quinoa, Spinach, Avocado Fry 2',
+        servingSize: 0,
+        servings: 0,
+        calories: 0,
+        ingredients: [
+            {
+                name: 'Quinoa',
+                unit: 'cup',
+                quantity: 0.5,
+                optional: false,
+            },
+            {
+                name: 'Spinach',
+                unit: 'oz',
+                quantity: 4,
+                optional: false,
+            },
+            {
+                name: 'Avocado',
+                unit: 'item',
+                quantity: 1,
+                optional: false,
+            },
+            {
+                name: 'Frozen Peas',
+                unit: 'cup',
+                quantity: 1.5,
+                optional: false,
+            },
+            {
+                name: 'Capers',
+                unit: 'Tbsp',
+                quantity: 1,
+                optional: true,
+            },
+            {
+                name: 'Shreaded Chicken',
+                unit: 'cup',
+                quantity: 0.75,
+                optional: true,
+            },
+            {
+                name: 'Teriaki Sauce',
+                unit: 'Tbsp',
+                quantity: 3,
+                optional: true,
+            },
+            {
+                name: 'Salt',
+                unit: 'tsp',
+                quantity: 0.5,
+                optional: false,
+            },
+            {
+                name: 'Pepper',
+                unit: 'tsp',
+                quantity: 1,
+                optional: false,
+            },
+            {
+                name: 'Vegetable Oil',
+                unit: 'Tbsp',
+                quantity: 2,
+                optional: false,
+            },
+            {
+                name: 'Feta Cheese',
+                unit: 'Tbsp',
+                quantity: 2,
+                optional: true,
+            },
+        ],
+        instructions: [
+            'Cook the quinoa according to the package instructions.',
+            'Defrost peas by running under warm water.',
+            'Combine quinoa and peas, set aside.',
+            'In a large skillet, heat oil over medium heat.',
+        ],
+    },
+    {
+        name: 'Quinoa, Spinach, Avocado Fry 3',
+        servingSize: 0,
+        servings: 0,
+        calories: 0,
+        ingredients: [
+            {
+                name: 'Quinoa',
+                unit: 'cup',
+                quantity: 0.5,
+                optional: false,
+            },
+            {
+                name: 'Spinach',
+                unit: 'oz',
+                quantity: 4,
+                optional: false,
+            },
+            {
+                name: 'Avocado',
+                unit: 'item',
+                quantity: 1,
+                optional: false,
+            },
+            {
+                name: 'Frozen Peas',
+                unit: 'cup',
+                quantity: 1.5,
+                optional: false,
+            },
+            {
+                name: 'Capers',
+                unit: 'Tbsp',
+                quantity: 1,
+                optional: true,
+            },
+            {
+                name: 'Shreaded Chicken',
+                unit: 'cup',
+                quantity: 0.75,
+                optional: true,
+            },
+            {
+                name: 'Teriaki Sauce',
+                unit: 'Tbsp',
+                quantity: 3,
+                optional: true,
+            },
+            {
+                name: 'Salt',
+                unit: 'tsp',
+                quantity: 0.5,
+                optional: false,
+            },
+            {
+                name: 'Pepper',
+                unit: 'tsp',
+                quantity: 1,
+                optional: false,
+            },
+            {
+                name: 'Vegetable Oil',
+                unit: 'Tbsp',
+                quantity: 2,
+                optional: false,
+            },
+            {
+                name: 'Feta Cheese',
+                unit: 'Tbsp',
+                quantity: 2,
+                optional: true,
+            },
+        ],
+        instructions: [
+            'Cook the quinoa according to the package instructions.',
+            'Defrost peas by running under warm water.',
+            'Combine quinoa and peas, set aside.',
+            'In a large skillet, heat oil over medium heat.',
+        ],
+    },
+];
+
+export default recipes;
